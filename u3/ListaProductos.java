c99b0aeab9bf7af60038bfdbd12b9aee0e47302d
“MemoOrtizgit config --global user.email “memodor2001@gmail.com <memodor2001@gmail.com>
2023-10-18 00:06:43 -0600

    Se terminó la clase ListaProductos

--- u3/ListaProductos.java
+++ u3/ListaProductos.java
@@ -11,6 +11,21 @@ public class ListaProductos {
             }
             listaProductos.addLast(listaProductos.removeFirst());
         }
+        return productoCaro;
+    }
+    //producto mas barato
+    public Producto productoBarato(SinglyLinkedList<Producto>listaProductos){
+        double precioBarato = listaProductos.last().getPrecio();
+        Producto productoBarato = null;
+        int cantidadNodos = listaProductos.size();
+        for(int contador =0;contador<=cantidadNodos;contador++){
+            if(listaProductos.first().getPrecio() < precioBarato){
+                precioBarato = listaProductos.first().getPrecio();
+                productoBarato = listaProductos.first();
+            }
+            listaProductos.addLast(listaProductos.removeFirst());
+        }
+        return productoBarato;
     }
 
 }
